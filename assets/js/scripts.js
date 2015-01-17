c94808461d8f3e82d83cd0af582cbca9d130b1ce
James Wu <jwu@efunda.com>
2015-01-17 00:57:51 -0800

    angular charts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,8 +2,17 @@ var date = new Date();
 date.toISOString();
 
 var last_update = date;
-window.onload(function() {
-});
 
 function pull_data(req, res) {
 }
+
+angular.module("app", ["chart.js"]).controller("LineCtrl", function($scope) {
+  $scope.labels = ["a", "b", "c", "d", "e", "f", "g"];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function(points, evt) {
+    console.log(points, evt);
+  };
+});
