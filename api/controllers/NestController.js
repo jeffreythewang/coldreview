8a1d41e40ef94f87fa9fa140cd8e0cc525e7cf39
Daniel Duan <danielduan88@yahoo.com>
2015-01-17 03:24:27 -0500

    get and set temperature working

--- api/controllers/NestController.js
+++ api/controllers/NestController.js
@@ -10,28 +10,44 @@ var nestApi = new NestApi('dduan@yahoo.com', '123456');
 
 var unoffNest = require('unofficial-nest-api');
 
-
 module.exports = {
 	login: function (req, res) {
 		nestApi.login(function(data) {
-			return res.send("done");
+			// return res.send("done");
 		});
+
+
 	},
 	set: function (req, res) {
 		var temp = req.params.temp;
-		settings = {
-			'target_change_pending': true,
-			'target_temperature': (temp - 30) / 2
-		}
-		nestApi.post(settings, function(data) {
-			return res.send(data);
+		unoffNest.login("dduan@yahoo.com", "123456", function (err, data) {
+			if (err) {
+				console.log(err.message);
+				return res.send('error');
+			}
+			unoffNest.fetchStatus(function (data) {
+				for (var deviceId in data.device) {
+					if (data.device.hasOwnProperty(deviceId)) {
+						var device = data.shared[deviceId];
+						// here's the device and ID
+						unoffNest.setTemperature(deviceId, unoffNest.ftoc(temp));
+					}
+				}
+				unoffNest.fetchStatus(function (data) {
+					return res.send(data.device);
+				});
+			});
 		});
 	},
 	get: function (req, res) {
-		this.login();
-		nestApi.get(function(data) {
-			return res.send(data.device);
+		unoffNest.login("dduan@yahoo.com", "123456", function (err, data) {
+			if (err) {
+				console.log(err.message);
+				return res.send('error');
+			}
+			unoffNest.fetchStatus(function (data) {
+				return res.send(data.device);
+			});
 		});
-
 	}
 };
