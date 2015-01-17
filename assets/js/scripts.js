319fa7079ac7d3f1637755d3de826cd4a4baa728
Jeff Wang <jeffreywang93@gmail.com>
2015-01-17 15:19:37 -0500

    more angles with angular

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,9 @@ var last_update = date;
 function pull_data(req, res) {
 }
 
-angular.module("app", ["chart.js"]).controller("LineCtrl", function($scope, $http) {
+var app = angular.module("app", ["chart.js"]);
+
+app.controller("LineCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.labels = ["a", "b", "c", "d", "e", "f", "g"];
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
@@ -15,37 +17,48 @@ angular.module("app", ["chart.js"]).controller("LineCtrl", function($scope, $htt
     console.log(points, evt);
   };
 
-  // do http requests in angular
-  // update after 3 seconds (via http request)
-  // need to loop this
-  setTimeout(function() {
-      $scope.$apply(function() {
-          $http.get('github/getCommits', {
-              param: {
-                  user: "jeffreythewang",
-                  repo: "superprod"
-              }
-          }).success(function(data) {
-              // create points for the scope data
+    $scope.repoUrl ='';
+    $scope.loadData = function() {
+        var url = $scope.repoUrl.split('/');
+        var ghIndex = url.indexOf('github.com');
+        $scope.userName = url[ghIndex+1];
+        $scope.repoName = url[ghIndex+2];
+        console.log(url);
+        console.log($scope.userName);
+        console.log($scope.repoName);
+        // do http requests in angular
+        // update after 3 seconds (via http request)
+        // need to loop this
+        setTimeout(function() {
+          $scope.$apply(function() {
+              $http.get('github/getCommits', {
+                  param: {
+                      user: $scope.userName,
+                      repo: $scope.repoName
+                  }
+              }).success(function(data) {
+                  // create points for the scope data
 
-              var numCommits = data.length;
+                  var numCommits = data.length;
 
-              console.log($scope.data);
-              var x_values = $scope.data[1];
-              x_values.shift();
-              x_values.push(numCommits);
+                  console.log($scope.data);
+                  var x_values = $scope.data[1];
+                  x_values.shift();
+                  x_values.push(numCommits);
 
-              // do the y_values with new Date
-              // store in some $scope var?
+                  // do the y_values with new Date
+                  // store in some $scope var?
 
-              for (var i = 0; i < numCommits; i++) {
-              }
+                  for (var i = 0; i < numCommits; i++) {
+                  }
 
-              console.log(numCommits);
-              console.log(data);
-          }).error(function() {
-              // do something here
+                  console.log(numCommits);
+                  console.log(data);
+              }).error(function() {
+                  // do something here
+              });
           });
-      });
-  }, 3000);
-});
+        }, 3000);
+        // do something
+    };
+}]);
