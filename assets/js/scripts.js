7b3bed794d919ac740939ee7ab88e96e2e29663f
Jeff Wang <jeffreywang93@gmail.com>
2015-01-17 16:30:02 -0500

    angular bro, don't mess with api rate limit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ function pull_data(req, res) {
 
 var app = angular.module("app", ["chart.js"]);
 
-app.controller("LineCtrl", ['$scope', '$http', function($scope, $http) {
+app.controller("LineCtrl", ['$scope', '$http', '$interval', function($scope, $http, $interval) {
   $scope.labels = ["a", "b", "c", "d", "e", "f", "g"];
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
@@ -16,6 +16,8 @@ app.controller("LineCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.onClick = function(points, evt) {
     console.log(points, evt);
   };
+
+  $scope.updateSpeed = 3000;
 
     $scope.repoUrl ='';
     $scope.loadData = function() {
@@ -26,11 +28,15 @@ app.controller("LineCtrl", ['$scope', '$http', function($scope, $http) {
         console.log(url);
         console.log($scope.userName);
         console.log($scope.repoName);
+        // do something
+        $scope.sendRequest();
+    };
+
+    $scope.sendRequest = function () {
         // do http requests in angular
         // update after 3 seconds (via http request)
         // need to loop this
-        setTimeout(function() {
-          $scope.$apply(function() {
+        $interval(function() {
               $http.get('github/getCommits', {
                   param: {
                       user: $scope.userName,
@@ -57,8 +63,14 @@ app.controller("LineCtrl", ['$scope', '$http', function($scope, $http) {
               }).error(function() {
                   // do something here
               });
-          });
-        }, 3000);
-        // do something
+        }, $scope.updateSpeed);
     };
+
+    $scope.updateFaster = function () {
+        if ($scope.updateSpeed > 5 * 1000) { $scope.updateSpeed -= 1000; }
+    }
+
+    $scope.updateSlower = function () {
+        if ($scope.updateSpeed <= 30 * 1000) { $scope.updateSpeed += 1000; }
+    }
 }]);
